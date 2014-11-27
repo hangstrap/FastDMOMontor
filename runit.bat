bea7283cb3877ac8b28d133daf8d4730dfbdc20a
unknown <Schmidt@MS008002.met.co.nz>
2014-11-27 17:11:19 +1300

    test

--- runit.bat
+++ runit.bat
@@ -1,2 +1,2 @@
-cd C:\Users\Schmidt\Documents\GitHub\FastDMOMontor
+cd C:\Users\Schmidt\Documents\GitHub\FastDMOMontor 
 C:\Users\Schmidt\dart\dart-sdk\bin\dart.exe C:\Users\Schmidt\Documents\GitHub\FastDMOMontor\bin\fastdmomontor.dart
